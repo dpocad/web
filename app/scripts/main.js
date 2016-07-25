993e4f41c67381f15805fa42cd2115f8ffab3826
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-07-25 16:45:08 +0300

    add mourn message

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,5 +6,10 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    // open initial modals
+    $('.modal.initial-open').each(function() {
+      $(this).openModal()
+    })
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
