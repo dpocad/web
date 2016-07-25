0cd44e9239e79e6cb3edf548ccadc694e4f42377
İsmail Demirbilek <ce.demirbilek@gmail.com>
2016-07-25 17:20:37 +0300

    Update 2016-07-25T14:20:00.317Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-!function(a){"use strict";a(function(){a(".button-collapse").sideNav(),a(".parallax").parallax()})}(jQuery);
+!function(a){"use strict";a(function(){a(".button-collapse").sideNav(),a(".parallax").parallax(),a(".modal.initial-open").each(function(){a(this).openModal()})})}(jQuery);
